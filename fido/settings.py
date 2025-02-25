c3f5134daf5265732c9a0d8e4ecdea08d4482db9
Eduardo Cardenaz <eduardoc_97@hotmail.com>
2025-02-25 17:07:00 -0600

    feat: django project running

--- fido/settings.py
+++ fido/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = "django-insecure-+r@7-w=htzl1c9mo^u3yw0)ab%q85ry8j&hbee*!!2_zi+5809
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["0.0.0.0"]
 
 
 # Application definition
